f0ca4202c6fb27dcaec5a7becf6d20aefeb5c880
Peter Schaeffer <schaeffer.peter9@gmail.com>
2023-01-27 15:02:48 +0100

    update dependencies

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.7
 
 import PackageDescription
 
 let package = Package(
     name: "GAppAuth",
     platforms: [
-        .macOS(.v10_11),
+        .macOS(.v10_13),
         .iOS(.v11)
     ],
     products: [
@@ -13,11 +13,19 @@ let package = Package(
         .library(name: "GAppAuth-macOS", targets: ["GAppAuth-macOS"])
     ],
     dependencies: [
-        .package(url: "https://github.com/google/GTMAppAuth.git", from: "1.0.0"),
-        .package(url: "https://github.com/openid/AppAuth-iOS.git", from: "1.0.0")
+        .package(url: "https://github.com/google/GTMAppAuth.git", from: "2.0.0"),
+        .package(url: "https://github.com/openid/AppAuth-iOS.git", from: "1.6.0")
     ],
     targets: [
-        .target(name: "GAppAuth-iOS", dependencies: ["GTMAppAuth", "AppAuth"], path: "Sources/iOS"),
-        .target(name: "GAppAuth-macOS", dependencies: ["GTMAppAuth", "AppAuth"], path: "Sources/macOS")
+        .target(name: "GAppAuth-iOS",
+                dependencies: [
+                    .product(name: "AppAuth", package: "AppAuth-iOS"),
+                    .product(name: "GTMAppAuth", package: "GTMAppAuth"),
+                ], path: "Sources/iOS"),
+        .target(name: "GAppAuth-macOS",
+                dependencies: [
+                    .product(name: "AppAuth", package: "AppAuth-iOS"),
+                    .product(name: "GTMAppAuth", package: "GTMAppAuth"),
+                ], path: "Sources/macOS")
     ]
 )
